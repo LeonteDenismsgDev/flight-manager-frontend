b66d686cf076a3597a710841b05da8fd5f8e2fb4
LeonteDenismsgDev <139860195+LeonteDenismsgDev@users.noreply.github.com>
2024-06-26 12:04:31 +0300

    append to last commit

--- src/app/landing/components/landing/landing.component.ts
+++ src/app/landing/components/landing/landing.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserDataService } from 'src/app/settings/services/user-data.service';
+import { UserDataRequest } from 'src/app/user/models/UserDataRequest';
+import { UserDataResponse } from 'src/app/user/models/UserDataResponse';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
-  firstName:string="Denis"
-  lastName:string="Leonte"
+export class LandingComponent implements OnInit{
+  firstName:string=""
+  lastName:string=""
+
+  username=localStorage.getItem("username");
+
+  constructor(private service:UserDataService){}
+
+  ngOnInit(): void {
+    this.service.getUserData(new UserDataRequest(this.username)).subscribe((userDataResponse:UserDataResponse)=>{
+      this.firstName=userDataResponse.firstName;
+      this.lastName=userDataResponse.lastName;
+    }); 
+  }
 }
